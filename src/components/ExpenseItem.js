6bc5020712b644c3b864c4f98198d7ace9002e3d
Chandanpreet Kaur <69481002+Chandanpreet91@users.noreply.github.com>
2021-12-13 14:50:47 -0800

    Removed the hard coded data and passes through props to child component.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,14 @@
 import './ExpenseItem.css';
-function ExpenseItem() {
-    return (
-        <div className="expense-item">
-        <div>Dec 11, 2021</div>
-        <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$293.5</div>
-                </div>
-        </div>
-    )
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
