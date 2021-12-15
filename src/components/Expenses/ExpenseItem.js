a3173aaba520cdb59f063eaa3c67b798d31d9d06
Chandanpreet Kaur <69481002+Chandanpreet91@users.noreply.github.com>
2021-12-15 13:46:25 -0800

    Created a button for changing state.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const updateTitle = () => {
+    setTitle("Updated Title!!");
+    console.log("updated title button clicked");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={updateTitle}>Click Me!!</button>
       </div>
     </Card>
   );
